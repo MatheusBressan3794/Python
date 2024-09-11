a45c2ddd4d361a29a989eba4592aa878f3d64226
MATHEUS BRESSAN VILA NOVA <025775@fatecararas.com>
2024-09-11 09:19:08 -0300

    mudança no codigo

--- PYTHON_no_cursoemvideo/desafios/ex022.py
+++ PYTHON_no_cursoemvideo/desafios/ex022.py
@@ -5,3 +5,4 @@ print('{}'.format(nome.lower()))
 print('Seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
 separa = nome.split()
 print('E {} tem {} letras'.format(separa[0], len(separa[0])))
+# Teste git
